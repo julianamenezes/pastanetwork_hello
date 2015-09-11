143e8838019b46782cc907740654b950e7f542b5
Larah <marialara.sp89@gmail.com>
2015-09-11 09:31:16 -0300

    add an author/email comment

--- ola.rb
+++ ola.rb
@@ -1,5 +1,31 @@
 puts "Hello, World"
+
 puts "Olá, # {} ARGV.first!"
 name = ARGV.first || "World"
 
 puts "Olá, # {name}!"
+name = ARGV.first || "World"
+
+puts "Olá, # {name}!"
+# This is a bad comment.  We want to revert it.
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+# This is an unwanted but staged comment
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+# This is an unwanted but committed change
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+# Default is World
+# Author: Jim Weirich
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
